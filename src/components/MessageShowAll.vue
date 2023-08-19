434b5423d8094fcedf8c9b373b51bfe2d88bec7e
conglinyizhi <cslbyivi@outlook.com>
2023-08-19 22:26:08 +0800

    fix: message 变量未被定义

--- src/components/MessageShowAll.vue
+++ src/components/MessageShowAll.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     全部对话内容
-    <div v-for="$message in messageArrayList" :key="message">
+    <div v-for="$message in messageArrayList" :key="$message">
       <span>{{ dataArray[$message.say].name }}：</span>
       <div class="editInput" v-for="message in $message.msg" :key="message">
         <span class="tag" v-if="message.type == 'tag'">{{ dataArray[message.tag].name }}</span>
