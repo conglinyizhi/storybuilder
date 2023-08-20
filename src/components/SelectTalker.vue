0f60a706926154948f8aaf9312a5f1724998ef8f
conglinyizhi <cslbyivi@outlook.com>
2023-08-20 17:06:33 +0800

    refactor: 将添加角色的按钮移动到角色列表

--- src/components/SelectTalker.vue
+++ src/components/SelectTalker.vue
@@ -1,36 +1,37 @@
 <template>
   <div v-if="!changeTalker">
-    当前发言角色：<v-chip prepend-icon="mdi-account-circle" append-icon="mdi-reload" @click="changeTalker = true">{{
-      dataArray[talkerId].name }}</v-chip>
+    <v-chip prepend-icon="mdi-account-circle" append-icon="mdi-reload" @click="changeTalker = true">
+      {{ data[talkerId].name }}
+    </v-chip>
   </div>
   <div v-else>
     请选择发言对象：
-    <v-chip-group active-class="primary-text" color="success" column v-model="chipSelectID">
-      <v-chip prepend-icon="mdi-account-circle" v-for="(talker, iId) in dataArray.slice(1)"
-        @click="changeTalkerId(iId + 1)" :value="iId + 1">
-        {{ talker.name }}
+    <v-chip-group active-class="primary-text" color="success" column v-model="chipSelectID" :readonly="true">
+      <v-chip prepend-icon="mdi-account-circle" v-for="(people, i) in data.slice(1)" @click="changeTalkerId(i + 1)"
+        :value="i">
+        {{ people.name }} - {{ i }}
       </v-chip>
-    </v-chip-group>
-    <div>或者，尝试
-      <v-chip @click="showCreateView = !showCreateView">
+      <v-chip prepend-icon="mdi-account-plus">
         {{ showCreateView ? '关闭窗口 X' : '添加角色 >' }}
       </v-chip>
-    </div>
-    <div v-show="showCreateView">
-      <people-create-view />
+    </v-chip-group>
+    <div v-show="chipSelectID == data.length - 1">
+      <v-text-field variant="filled" label="角色名称" v-model="nameInput"></v-text-field>
+      <v-btn color="primary" @click="addPeople">确认添加角色</v-btn>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import PeopleCreateView from './PeopleCreate.vue'
 import { useTalkConfig } from '../store/people'
 import { storeToRefs } from 'pinia'
+import { VChip } from 'vuetify/lib/components/index.mjs'
+import { VChipGroup } from 'vuetify/lib/components/index.mjs'
 
 const talkConfig = useTalkConfig()
 
-const { messageArray, dataArray, talkerId, select } = storeToRefs(talkConfig)
+const { messageArray, dataArray: data, talkerId, select } = storeToRefs(talkConfig)
 
 const showCreateView = ref(false)
 const changeTalker = ref(false)
@@ -39,5 +40,20 @@ const chipSelectID = ref(0)
 const changeTalkerId = (i) => {
   talkerId.value = i
   changeTalker.value = false
+}
+
+const nameInput = ref('')
+
+const addPeople = () => {
+  if (!nameInput.value.trim()) {
+    alert('请给阁下的角色一个名字，好吗')
+  } else {
+    dataArray.value.push({
+      name: nameInput.value,
+      group: 'default',
+      profile: ''
+    })
+    nameInput.value = ''
+  }
 }
 </script>
