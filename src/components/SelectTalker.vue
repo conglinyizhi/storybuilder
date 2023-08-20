c3fd584259f57843bb32b761b9851b3ca2520812
conglinyizhi <cslbyivi@outlook.com>
2023-08-20 16:47:02 +0800

    refactor: 对于已经选择的角色，将会在选择其他角色时被高亮

--- src/components/SelectTalker.vue
+++ src/components/SelectTalker.vue
@@ -1,11 +1,13 @@
 <template>
   <div v-if="!changeTalker">
-    当前发言角色：<v-chip prepend-icon="mdi-account-circle" @click="changeTalker = true">{{ dataArray[talkerId].name }}</v-chip>
+    当前发言角色：<v-chip prepend-icon="mdi-account-circle" append-icon="mdi-reload" @click="changeTalker = true">{{
+      dataArray[talkerId].name }}</v-chip>
   </div>
   <div v-else>
     请选择发言对象：
-    <v-chip-group active-class="primary--text" column>
-      <v-chip v-for="(talker, iId) in dataArray.slice(1)" @click="changeTalkerId(iId + 1)">
+    <v-chip-group active-class="primary-text" color="success" column v-model="chipSelectID">
+      <v-chip prepend-icon="mdi-account-circle" v-for="(talker, iId) in dataArray.slice(1)"
+        @click="changeTalkerId(iId + 1)" :value="iId + 1">
         {{ talker.name }}
       </v-chip>
     </v-chip-group>
@@ -32,6 +34,7 @@ const { messageArray, dataArray, talkerId, select } = storeToRefs(talkConfig)
 
 const showCreateView = ref(false)
 const changeTalker = ref(false)
+const chipSelectID = ref(0)
 
 const changeTalkerId = (i) => {
   talkerId.value = i
