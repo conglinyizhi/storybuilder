9ec39c5739c5a29f190b19b7b36948879cd1673e
conglinyizhi <cslbyivi@outlook.com>
2023-08-21 21:44:10 +0800

    style: IDE 自动格式化代码

--- src/router.js
+++ src/router.js
@@ -3,12 +3,12 @@ import MainEditView from './view/MainEditView.vue';
 const router = [{
   path: '/',
   component: MainEditView
-},{
+}, {
   path: '/about',
-  component: ()=>import('./view/AboutView.vue')
-},{
-  path:'/write',
-  component:()=>import('./view/WriterView.vue')
+  component: () => import('./view/AboutView.vue')
+}, {
+  path: '/write',
+  component: () => import('./view/WriterView.vue')
 }
 ];
 
