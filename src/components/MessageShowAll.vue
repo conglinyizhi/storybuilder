183baf35fa7e50835ae60aee3e569da4365b0291
conglinyizhi <cslbyivi@outlook.com>
2023-08-22 19:25:51 +0800

    fix: 删除消息会在生成文本再次出现的问题

--- src/components/MessageShowAll.vue
+++ src/components/MessageShowAll.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="messageItem" v-for="$message in messageArrayList" v-show="$message.msg.type !== 'remove'" :key="$message">
-    <v-chip class="nameTag" prepend-icon="mdi-account-circle" closable @click:close="removeMessageById($message)">{{
-      dataArray[$message.say].name }}</v-chip>
-    <div class="editInput" v-for="message in $message.msg" :key="message">
-      <v-chip class="tag" v-if="message.type == 'tag'">{{ dataArray[message.tag].name }}</v-chip>
-      <span class="edit" v-else>{{ message.text }}</span>
-    </div>
+  <div class="messageItem" v-for="($message, msgIndex) in messageArrayList" :key="$message">
+    <v-chip class="nameTag" prepend-icon="mdi-account-circle" closable @click:close="removeMessageByIndex(msgIndex)">
+      {{ dataArray[$message.say].name }}
+    </v-chip>
+    <span class="messageText">
+      <span class="editInput" v-for="message in $message.msg" :key="message">
+        <v-chip class="tag" v-if="message.type == 'tag'">{{ dataArray[message.tag].name }}</v-chip>
+        <span class="edit" v-else>{{ message.text }}</span>
+      </span>
+    </span>
   </div>
 </template>
   
@@ -18,8 +21,8 @@ const talkConfig = useTalkConfig()
 
 const { messageArray, dataArray, talkerId, select, messageArrayList, messageArrayIndex } = storeToRefs(talkConfig)
 
-const removeMessageById = (msg) => {
-  msg.msg.type = 'remove'
+const removeMessageByIndex = (msgIndex) => {
+  messageArrayList = messageArrayList.value.splice(msgIndex, 1)
 }
 </script>
 
@@ -27,8 +30,14 @@ const removeMessageById = (msg) => {
 .messageItem {
   margin: 1rem 0;
 
-  .nameTag {
-    margin-right: 1rem;
+  .messageText {
+    &.remove {
+      color: #888888;
+    }
+
+    .nameTag {
+      margin-right: 1rem;
+    }
   }
 
   &:hover {
