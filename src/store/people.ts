587b1aeaa0d281efdfb754694754cb3cb0cbd240
conglinyizhi <cslbyivi@outlook.com>
2023-08-27 22:45:21 +0800

    feat: 新增一种数据类型

--- src/store/people.ts
+++ src/store/people.ts
@@ -1,11 +1,17 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
+// 消息数组成员数据类型
 type MessageArrayItem = { type: 'text' | 'tag' | 'remove', tag: number | undefined, text: string | undefined }
 
+// 消息列表数组成员数据类型
 type MessageArrayListItem = { say: number, msg: MessageArrayItem[] }
 
+// 人物数组列表数据类型
 type PeopleItem = { name: string, group: string, profile: string }
+
+// 名称列表数据类型
+type nameListItem = { name: string, context: string }
 
 export const useTalkConfig = defineStore('talkConfig', function () {
     const messageArray = ref([{ type: 'text', text: '' }] as MessageArrayItem[]);
@@ -20,5 +26,8 @@ export const useTalkConfig = defineStore('talkConfig', function () {
             profile: ''
         }
     }) as PeopleItem[]);
-    return { messageArray, dataArray, talkerId, select, messageArrayList, messageArrayIndex }
+    // 其他需要快捷输入字符串的列表
+    const otherNameList = ref([] as nameListItem[])
+
+    return { messageArray, dataArray, talkerId, select, messageArrayList, messageArrayIndex, otherNameList }
 })
