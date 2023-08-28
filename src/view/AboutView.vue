84f90fd00fe332ac8ed1ff254a1f5e165f793073
conglinyizhi <cslbyivi@outlook.com>
2023-08-28 19:05:16 +0800

    refactor: 添加 Github 仓库链接

--- src/view/AboutView.vue
+++ src/view/AboutView.vue
@@ -1,3 +1,5 @@
 <template>
   Open Source is awesome. Powered by Vuetify and Vite
+
+  Github:<a href="https://github.com/conglinyizhi/storybuilder">https://github.com/conglinyizhi/storybuilder</a>
 </template>
